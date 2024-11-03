46ecc851cdb211f10b6e889129877ecc98f8376f
Saman Dehestani <drippypale@gmail.com>
2024-11-03 13:03:35 +0330

    feat(init): add new keybinding for telescope
    <leader>sl searches among all the git commit histories. [s]earch [l]ogs

--- init.lua
+++ init.lua
@@ -423,6 +423,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>sl', builtin.git_commits, { desc = '[S]earch git commits.' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
