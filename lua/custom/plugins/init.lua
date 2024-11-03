03a4dd24e8d1eee77eccfda20cc63639d47c40d2
Saman Dehestani <drippypale@gmail.com>
2024-11-03 12:47:06 +0330

    feat(custom): add barber plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- barber to add tabs
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      clickable = false,
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    keys = {
+      { '<Tab>', '<Cmd>BufferNext<CR>', desc = 'Go to the next buffer', silent = true, noremap = true },
+      { '<S-Tab>', '<Cmd>BufferPrevious<CR>', desc = 'Go to the prev buffer', silent = true, noremap = true },
+      { '<leader>x', '<Cmd>BufferClose<CR>', desc = 'Close the current buffer', silent = true, noremap = true },
+    },
+  },
+}
