89c3f20ea1f3e79e6ed93bb8234d9d263371b668
Saman Dehestani <drippypale@gmail.com>
2024-11-10 14:01:30 +0330

    feat(custom): add mapping for quick access the buffers in the harpoon config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -73,6 +73,20 @@ return {
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = 'Harpoon m[e]nu' })
 
+      -- Harpoon Buffer Selection using Leader key
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end)
+
       -- Toggle previous & next buffers stored within Harpoon list
       vim.keymap.set('n', '<C-[>', function()
         harpoon:list():prev { ui_nav_wrap = true }
